f2d6e0580dd9b5bc034bd4dc7d9a4508e16a3024
Alexandre Chartrand <alexchartrand91@gmail.com>
2016-06-11 14:56:14 -0400

    First version

--- Safety_stop/Safety_stop.ino
+++ Safety_stop/Safety_stop.ino
@@ -1,12 +1,13 @@
 /*
- * rosserial Publisher Example
- * Prints "hello world!"
+ * Arduino based safety stop
+ * Service: safety_stop_srv
+ * Topic: start_button
  */
 
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
-#include <std_srvs/Trigger.h>
+#include <std_srvs/SetBool.h>
 
 typedef enum{
   RUNNING =1,
@@ -17,26 +18,24 @@ const int START_BUTTON = 4;
 const int SAFETY_BUTTON = 5;
 const int DRIVE_CTRL = 6;
 const int POWER_CTRL = 7;
-const int LED_PIN = 13;
+const int GREEN_LED_PIN = 6;
+const int RED_LED_PIN = 11;
+const int INTERVAL = 500;
 
-unsigned long last_time = 0;
-const long INTERVAL = 500;
 int led_state = LOW; 
 int power_state = HIGH;
 State system_state;
 
-bool last_reading = false;
-unsigned long last_debounce_time=0;
-const unsigned long debounce_delay=50;
-bool published = true;
-
 ros::NodeHandle  nh;
 
-std_msgs::Bool alert_msg;
-ros::Publisher safety_stop("safety_stop", &alert_msg);
+ros::ServiceClient<std_srvs::SetBool::Request, std_srvs::SetBool::Response> safety_stop_srv("safety_stop_srv");
 
 std_msgs::Bool start_msg;
 ros::Publisher start_button("start_button", &start_msg);
+
+void start_button_handle();
+void safety_stop_handler();
+void led_handler();
 
 void setup()
 {
@@ -44,36 +43,91 @@ void setup()
   pinMode(SAFETY_BUTTON, INPUT);
   pinMode(DRIVE_CTRL, OUTPUT);
   pinMode(POWER_CTRL, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  
   digitalWrite(POWER_CTRL, power_state);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, led_state);
+  digitalWrite(GREEN_LED_PIN, HIGH);
   
   system_state = RUNNING;
-  start_msg.data = true;
+  
   nh.initNode();
-  nh.advertise(safety_stop);
   nh.advertise(start_button);
+  nh.serviceClient(safety_stop_srv);
+  
+  while(!nh.connected()) nh.spinOnce();
+  nh.loginfo("Safety stop startup complete");
 }
 
 void loop()
 {
+  if(system_state == RUNNING)
+  { 
+    start_button_handle();
+    led_handler();
+    safety_stop_handler();
+  }
   
-  // Start button publishing ------------------------------
-  if(system_state == RUNNING){
-    bool reading = digitalRead(START_BUTTON);
+  if(system_state == SAFETY_STOP)
+  {
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(RED_LED_PIN, HIGH);
+    safety_stop_handler();
+  }
   
-    if (last_reading!= reading){
-      last_reading = reading;
-      if(reading){
-        start_button.publish(&start_msg);
+  digitalWrite(POWER_CTRL, power_state);
+  nh.spinOnce();
+}
+
+void start_button_handle()
+{
+  static bool last_reading = false;
+  bool reading = digitalRead(START_BUTTON);
+  
+  if (last_reading!= reading){
+    last_reading = reading;
+    if(reading){
+      start_msg.data = reading;
+      start_button.publish(&start_msg);
+    }
+  }
+}
+
+void safety_stop_handler()
+{
+  static bool last_reading = true;
+  bool reading = digitalRead(SAFETY_BUTTON);
+  std_srvs::SetBool::Request req;
+  std_srvs::SetBool::Response res;
+  
+  if (last_reading!= reading){
+    last_reading = reading;
+    req.data = reading;
+    if(safety_stop_srv.call(req, res))
+    {
+      if(reading)
+      {
+        system_state = RUNNING;
+      }
+      else
+      {
+        system_state = SAFETY_STOP;
       }
     }
+    else
+    {
+      system_state = SAFETY_STOP;
+      power_state = LOW;
+    }
+  }
+}
+
+void led_handler()
+{
+  static unsigned long last_time = 0;
   
-  
-  // Led flashing (state dependant) -------------------------------------
-  if (millis() - last_time >= INTERVAL && system_state == RUNNING)
+  if (millis() - last_time >= INTERVAL)
   {
-    // save the last time you blinked the LED
     last_time = millis();
     
     if (led_state == LOW) {
@@ -81,19 +135,7 @@ void loop()
     } else {
       led_state = LOW;
     }
-    digitalWrite(LED_PIN, led_state);
+    digitalWrite(GREEN_LED_PIN, led_state);
   }
-  else{
-    digitalWrite(LED_PIN, HIGH);
-  }
-  
-  // Safety stop --------------------------------
-  if(digitalRead(SAFETY_BUTTON) == LOW){
-    system_state = SAFETY_STOP;
-    
-    //send service
-  }
-  
-  nh.spinOnce();
 }
 
